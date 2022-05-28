68b3ddb8f6a7c2651e77b0715c3427e25cb5b1e8
webdevyn <sendtodevyn@gmail.com>
2022-05-28 15:33:46 -0700

    "update changes exercises"

--- loop.py
+++ loop.py
@@ -1,4 +1,10 @@
-magicians=['alice','david','carolina']
-for magician in magicians:
-    print(f"{magician.title()}, that was a great trick!")
-    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+# magicians=['alice','david','carolina']
+# for magician in magicians:
+#     print(f"{magician.title()}, that was a great trick!")
+#     print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+pizzas=['margarita','greek','mushroom']
+for pizza in pizzas:
+    print(f"{pizza.title()} is a delicious pizza.")
+
+print(f"I really love pizza!")
