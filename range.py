0ebccb73bdf720af4274fdf75e60c720637d0a8d
webdevyn <sendtodevyn@gmail.com>
2022-05-29 16:11:31 -0700

    update range with slicing and copying lists

--- range.py
+++ range.py
@@ -38,5 +38,19 @@
 # threes=[num for num in range(3,31,3)]
 # print(threes)
 
-cubes=[num**3 for num in range(1,11)]
-print(cubes)
+# cubes=[num**3 for num in range(1,11)]
+# print(cubes)
+# print(f"The first three items are: {cubes[:3]}")
+# print(f"The middle three items are: {cubes[3:6]}")
+# print(f"The last three items are: {cubes[7:]}")
+
+pizzas = ["greek", "margarita", "mushroom"]
+friend_pizzas = pizzas[:]
+pizzas.append("vegetable")
+friend_pizzas.append("pepper")
+print(f"My favorite pizzas are: ")
+for pizza in pizzas:
+    print(pizza)
+print(f"My friend's favorite pizzas are: ")
+for each in friend_pizzas:
+    print(each)
