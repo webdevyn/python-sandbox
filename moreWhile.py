e28c1507470137b958743ca7743dc4d3b06b97ce
webdevyn <sendtodevyn@gmail.com>
2022-06-12 15:52:22 -0700

    working with while loops

---
+++ moreWhile.py
@@ -0,0 +1,43 @@
+# prompt = "\nTell me something, and I'll repeat it:"
+# prompt += "\nEnter 'quit' to exit.\n"
+
+# active = True
+# while active:
+#     message = input(prompt)
+
+#     if message == 'quit':
+#         active = False
+#     else:
+#         print(message)
+
+# prompt = "\nEnter a name of a city you've visited:"
+# prompt += "\nEnter 'quit' to exit.\n"
+
+# while True:
+#     city = input(prompt)
+
+#     if city == 'quit':
+#         break
+#     else:
+#         print(f"I'd love to go to {city.title()}!")
+
+# current_number = 0
+# while current_number < 10:
+#     current_number += 1
+#     if current_number % 2 == 0:
+#         continue   # if even num, ignore rest of loop
+
+#     print(current_number)  # only prints if num is odd
+
+
+# prompt = "\nEnter a pizza topping:"
+# prompt += "\n(Enter 'quit' to exit)\n"
+
+# while True:
+#     topping = input(prompt)
+
+#     if topping == 'quit':
+#         break
+#     else:
+#         print(f"{topping.title()} has been added to your pizza")
+
