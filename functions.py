e71a1955b863b9f650af0796185195e8bdfb505c
webdevyn <sendtodevyn@gmail.com>
2022-06-18 14:59:38 -0700

    working with functions

---
+++ functions.py
@@ -0,0 +1,19 @@
+
+# def display_message():
+#     """Displays message about learning functions."""
+#     print("This chapter covers functions.")
+
+# display_message()
+
+# def fav_book(book_input):
+#     """Displays favorite book"""
+#     print(f"{book_input.title()} is my favorite book.")
+
+# fav_book("sherlock holmes")
+
+# def make_shirt(size="L", text="I love Python"):
+#     print(f"Shirt size is {size} and says {text}.")
+
+# make_shirt("M", "smile it's Friday")
+# make_shirt()
+
