21e2f711f621fc679178a4e7792c0d3444e84c86
webdevyn <sendtodevyn@gmail.com>
2022-06-24 19:57:45 -0700

    functions with input

--- functions.py
+++ functions.py
@@ -16,3 +16,46 @@
 
 # make_shirt("M", "smile it's Friday")
 # make_shirt()
+
+
+# def get_formatted_name(first_name, last_name):
+#     """Return a full name, neatly formatted"""
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+
+# while True:
+#     print("\nEnter your name:")
+#     print("\n(enter 'q' to quit)")
+
+#     f_name = input("First name: ")
+#     if f_name == 'q':
+#         break
+
+#     l_name = input("Last name: ")
+#     if l_name == 'q':
+#         break
+
+#     formatted_name = get_formatted_name(f_name, l_name)
+#     print(f"\nHello, {formatted_name}!")
+    
+
+
+def city_country(city, country):
+    """Return a city and country"""
+    city_country_full = f"{city}, {country}"
+    return city_country_full.title()
+
+while True:
+    print("Enter a city: ")
+    print("(q to quit)")
+
+    city_name = input("City: ")
+    if city_name == 'q':
+        break
+
+    country_name = input("Country: ")
+    if country_name == 'q':
+        break
+
+    formatted = city_country(city_name, country_name)
+    print(f"{formatted} is a great place!\n")
