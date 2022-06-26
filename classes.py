f6bd875056454a365090eb53dff4d8bd7acb4a96
webdevyn <sendtodevyn@gmail.com>
2022-06-25 18:39:27 -0700

    working with classes

---
+++ classes.py
@@ -0,0 +1,25 @@
+class Dog:
+    """a simple model of a dog"""
+    def __init__(self,name,age):
+        """initialize name and age attributes"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """simulate a dog sitting in response to a command"""
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        """simulate rolling over in response to a command"""
+        print(f"{self.name} rolled over!")
+
+my_dog = Dog('Willie', 6)
+your_dog = Dog("Lucy", 3)
+
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+
+print(f"\nYour dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
