45952e390bde32827f5b2d4224e441205d972228
webdevyn <sendtodevyn@gmail.com>
2022-07-02 16:16:57 -0700

    practice class inheritance

---
+++ child_class.py
@@ -0,0 +1,65 @@
+class Car:
+    """Simple representation of a car"""
+
+    def __init__(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name=f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
+    
+    def update_odometer(self,mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("Error - can't roll back odometer.")
+    
+    def increment_odometer(self,miles):
+        self.odometer_reading += miles
+
+class Battery:
+    """Model electric car battery"""
+
+    def __init__(self,battery_size=75):
+        """Initialize battery attributes"""
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        """Print stmt describing battery size"""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+    
+    def get_range(self):
+        """Print stmt about battery range"""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+        
+        print(f"This car can go {range} miles on a full charge.")
+
+class ElectricCar(Car):
+    """Represent aspects of electric car"""
+
+    def __init__(self,make,model,year):
+        """Initialize attributes of the parent class"""
+        super().__init__(make,model,year)
+        """Initialize electric car attributes"""
+        self.battery = Battery()
+
+    def fill_gas_tank(self):
+        """does not apply to electric cars"""
+        print("This car doesn't need gas.")
+
+my_500e = ElectricCar('fiat','model 500e',2019)
+print(my_500e.get_descriptive_name())
+print(f"{my_500e.odometer_reading} current miles.")
+my_500e.increment_odometer(5000)
+print(f"New mileage is: {my_500e.odometer_reading}")
+my_500e.battery.describe_battery()
+my_500e.battery.get_range()
